442337842cb88603f7c8745843ceff49c78050ab
infastin <infastin@yandex.com>
2024-10-06 17:36:39 +0500

    feat: added NotFound and MethodNotAllowed middlewares

---
+++ xrest/not_found.go
@@ -0,0 +1,40 @@
+package xrest
+
+import "net/http"
+
+type notFoundWriter struct {
+	http.ResponseWriter
+	request    *http.Request
+	statusCode int
+	handler    http.HandlerFunc
+}
+
+func (w *notFoundWriter) WriteHeader(statusCode int) {
+	if statusCode == http.StatusNotFound {
+		w.statusCode = statusCode
+		return
+	}
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *notFoundWriter) Write(data []byte) (int, error) {
+	if w.statusCode == http.StatusNotFound {
+		w.handler(w.ResponseWriter, w.request)
+		return 0, nil
+	}
+	return w.ResponseWriter.Write(data)
+}
+
+func NotFound(handler http.HandlerFunc) Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			nw := &notFoundWriter{
+				ResponseWriter: w,
+				request:        r,
+				statusCode:     http.StatusOK,
+				handler:        handler,
+			}
+			next.ServeHTTP(nw, r)
+		})
+	}
+}
