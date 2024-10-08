6480e5c33de32fbdb282db074a2b8e782ba481c5
infastin <infastin@yandex.com>
2024-10-08 17:05:43 +0500

    feat: use rs/cors for CORS

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/infastin/gorack
 
 go 1.23.0
 
-require gopkg.in/yaml.v3 v3.0.1
+require (
+	github.com/rs/cors v1.11.1
+	gopkg.in/yaml.v3 v3.0.1
+)
--- xrest/cors.go
+++ xrest/cors.go
@@ -1,47 +1,15 @@
 package xrest
 
 import (
-	"net/http"
-	"strings"
+	"github.com/rs/cors"
 )
 
-type CORSConfig struct {
-	AllowOrigin  string
-	AllowMethods []string
-}
-
-var DefaultConfig = CORSConfig{
-	AllowOrigin: "*",
-	AllowMethods: []string{
-		http.MethodGet,
-		http.MethodHead,
-		http.MethodPut,
-		http.MethodPatch,
-		http.MethodPost,
-		http.MethodDelete,
-	},
-}
+type CORSOptions = cors.Options
 
 func CORS() Middleware {
-	return CORSWithConfig(DefaultConfig)
+	return cors.Default().Handler
 }
 
-func CORSWithConfig(config CORSConfig) Middleware {
-	if config.AllowOrigin == "" {
-		config.AllowOrigin = DefaultConfig.AllowOrigin
-	}
-	if config.AllowMethods == nil {
-		config.AllowMethods = DefaultConfig.AllowMethods
-	}
-
-	allowOrigin := config.AllowOrigin
-	allowMethods := strings.Join(config.AllowMethods, ",")
-
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Add("Access-Control-Allow-Origin", allowOrigin)
-			w.Header().Add("Access-Control-Allow-Methods", allowMethods)
-			next.ServeHTTP(w, r)
-		})
-	}
+func CORSWithOptions(options CORSOptions) Middleware {
+	return cors.New(options).Handler
 }
