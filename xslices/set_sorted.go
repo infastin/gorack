c58e84e88c03d25610f84ba30c59326e640a5554
infastin <infastin@yandex.com>
2025-05-05 10:07:23 +0500

    docs(xslices): change documentation

--- xslices/set_sorted.go
+++ xslices/set_sorted.go
@@ -8,45 +8,38 @@ import (
 )
 
 // Computes union of two sorted slices.
-// Reference: https://en.cppreference.com/w/cpp/algorithm/set_union.
 func SetUnion[E cmp.Ordered, Slice ~[]E](s1, s2 Slice) Slice {
 	return slices.Collect(xalg.SetUnion(slices.Values(s1), slices.Values(s2)))
 }
 
-// Computes union of two sorted slices.
-// Reference: https://en.cppreference.com/w/cpp/algorithm/set_union.
+// Computes union of two sorted slices using a comparison function.
 func SetUnionFunc[E any, Slice ~[]E](s1, s2 Slice, comp func(E, E) int) Slice {
 	return slices.Collect(xalg.SetUnionFunc(slices.Values(s1), slices.Values(s2), comp))
 }
 
 // Computes intersection of two sorted slices.
-// Reference: https://en.cppreference.com/w/cpp/algorithm/set_intersection.
 func SetIntersection[E cmp.Ordered, Slice ~[]E](s1, s2 Slice) Slice {
 	return slices.Collect(xalg.SetIntersection(slices.Values(s1), slices.Values(s2)))
 }
 
-// Computes intersection of two sorted slices.
-// Reference: https://en.cppreference.com/w/cpp/algorithm/set_intersection.
+// Computes intersection of two sorted slices using a comparison function.
 func SetIntersectionFunc[E any, Slice ~[]E](s1, s2 Slice, comp func(E, E) int) Slice {
 	return slices.Collect(xalg.SetIntersectionFunc(slices.Values(s1), slices.Values(s2), comp))
 }
 
 // Returns a slice that contains elements from the first sorted slice
 // that are not found in the second sorted slice.
-// Reference: https://en.cppreference.com/w/cpp/algorithm/set_difference.
 func SetDifference[E cmp.Ordered, Slice ~[]E](s1, s2 Slice) Slice {
 	return slices.Collect(xalg.SetDifference(slices.Values(s1), slices.Values(s2)))
 }
 
 // Returns a slice that contains elements from the first sorted slice
-// that are not found in the second sorted slice.
-// Reference: https://en.cppreference.com/w/cpp/algorithm/set_difference.
+// that are not found in the second sorted slice using a comparison function.
 func SetDifferenceFunc[E any, Slice ~[]E](s1, s2 Slice, comp func(E, E) int) Slice {
 	return slices.Collect(xalg.SetDifferenceFunc(slices.Values(s1), slices.Values(s2), comp))
 }
 
 // Computes symmetric difference of two sorted slices.
-// Reference: https://en.cppreference.com/w/cpp/algorithm/set_symmetric_difference.
 func SetSymmetricDifference[E cmp.Ordered, Slice ~[]E](s1, s2 Slice) (first, second Slice) {
 	for elem, typ := range xalg.SetSymmetricDifference(slices.Values(s1), slices.Values(s2)) {
 		switch typ {
@@ -59,8 +52,7 @@ func SetSymmetricDifference[E cmp.Ordered, Slice ~[]E](s1, s2 Slice) (first, sec
 	return first, second
 }
 
-// Computes symmetric difference of two sorted slices.
-// Reference: https://en.cppreference.com/w/cpp/algorithm/set_symmetric_difference.
+// Computes symmetric difference of two sorted slices using a comparison function.
 func SetSymmetricDifferenceFunc[E any, Slice ~[]E](s1, s2 Slice, comp func(E, E) int) (first, second Slice) {
 	for elem, typ := range xalg.SetSymmetricDifferenceFunc(slices.Values(s1), slices.Values(s2), comp) {
 		switch typ {
