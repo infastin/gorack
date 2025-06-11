864c49ad01aa0f25e0723acad7827194fde4c239
infastin <infastin@yandex.com>
2025-06-11 23:01:22 +0500

    feat(constraints): add more constraints

--- constraints/constraints.go
+++ constraints/constraints.go
@@ -1,21 +1,29 @@
 package constraints
 
+import "cmp"
+
 type Int interface {
 	~int8 | ~int16 | ~int32 | ~int64 | ~int
 }
+
+type Signed = Int
 
 type Uint interface {
 	~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uint | ~uintptr
 }
 
+type Unsigned = Uint
+
 type Float interface {
 	~float32 | ~float64
 }
 
-type Number interface {
-	Int | Uint | Float
+type Complex interface {
+	~complex64 | ~complex128
 }
 
-type Ordered interface {
-	~string | Int | Uint | Float
+type Number interface {
+	Int | Uint | Float | Complex
 }
+
+type Ordered = cmp.Ordered
