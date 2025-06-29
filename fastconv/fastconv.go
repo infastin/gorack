57d04e946003a775fc0632817f6d12329ea825a6
infastin <infastin@yandex.com>
2025-06-29 20:31:00 +0500

    feat(fastconv): add SliceSafe

--- fastconv/fastconv.go
+++ fastconv/fastconv.go
@@ -1,23 +1,25 @@
 package fastconv
 
 import (
+	"fmt"
+	"reflect"
 	"unsafe"
 )
 
-// Fast conversion of a byte slice to a string.
+// String converts a byte slice into a string without extra allocations.
 // The bytes argument can be nil.
 // Must not be used if the byte slice could be mutated.
-func String[B ~[]byte](bytes B) string {
-	return unsafe.String(unsafe.SliceData(bytes), len(bytes))
+func String[B ~[]byte](b B) string {
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
 
-// Fast conversion of a string to a byte slice.
+// Bytes converts a string into a byte slice without extra allocations.
 // Must not be used if the resulting byte slice could be mutated.
-func Bytes[S ~string](str S) []byte {
-	return unsafe.Slice(unsafe.StringData(string(str)), len(str))
+func Bytes[S ~string](s S) []byte {
+	return unsafe.Slice(unsafe.StringData(string(s)), len(s))
 }
 
-// Returns a type pointer.
+// TypePointer returns a type pointer.
 func TypePointer(a any) uintptr {
 	type emptyInterface struct {
 		typ unsafe.Pointer
@@ -27,9 +29,19 @@ func TypePointer(a any) uintptr {
 	return uintptr(iface.typ)
 }
 
-// Fast conversion of a slice of one type to a slice of another type
-// without copying. Use only if type I can be converted to type O.
-func Slice[O, I any](input []I) (output []O) {
-	outputPtr := (*O)(unsafe.Pointer(unsafe.SliceData(input)))
+// Slice converts a slice of one type to a slice of another type without extra allocations.
+// Use only when type In can be converted to type Out.
+func Slice[Out, In any, InSlice ~[]In](input InSlice) (output []Out) {
+	outputPtr := (*Out)(unsafe.Pointer(unsafe.SliceData(input)))
+	return unsafe.Slice(outputPtr, len(input))
+}
+
+// SliceSafe converts a slice of one type to a slice of another type without extra allocations.
+// Will panic if type In can't be converted to type Out.
+func SliceSafe[Out, In any, InSlice ~[]In](input InSlice) (output []Out) {
+	if itp, otp := reflect.TypeFor[In](), reflect.TypeFor[Out](); itp.ConvertibleTo(otp) {
+		panic(fmt.Sprintf("fastconv: %T is not convertible to %T", itp, otp))
+	}
+	outputPtr := (*Out)(unsafe.Pointer(unsafe.SliceData(input)))
 	return unsafe.Slice(outputPtr, len(input))
 }
